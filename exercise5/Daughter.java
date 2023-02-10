83eac62b306e3b5a5673c80178af0410b5db7321
Katherine Espinosa <katherinemaegespinosa@gmail.com>
2023-02-10 14:29:29 +0800

    Add files via upload

---
+++ exercise5/Daughter.java
@@ -0,0 +1,7 @@
+public class Daughter extends Papa {
+
+    
+    
+
+
+}
---
+++ exercise5/Papa.java
@@ -0,0 +1,9 @@
+public class Papa {
+    
+    protected String surname = "Espinosa";
+    
+    protected void showSurname (){
+        System.out.println("My family surname is: "+ surname);
+    }
+
+}
